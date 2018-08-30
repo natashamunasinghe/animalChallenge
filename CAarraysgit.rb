e8cba438f0757f321ee7fa7cfdc0a898d37fcc89
Your Name <you@example.com>
2018-08-30 16:55:02 +1000

    add arrays challenge 1

---
+++ CAarraysgit.rb
@@ -0,0 +1,44 @@
+#Create an array called four_letter_animals, inorder 
+#[,Duck,Elephant,Goat,Lamb,Lion,Mule,Dog]
+#Add Puma to the end of the array
+#Insert Joey after Goat and before Lamb
+#Delete Dog
+#Reverse the order of the array
+#Replace Elephant with Foal
+#Add Bear to the end of the array
+#Reverse the order of the array again
+
+four_letter_animals = ['', 'Duck', 'Elephant', 'Goat', 'Lamb', 'Lion', 'Mule', 'Dog']
+
+  #puts four_letter_animals
+
+  #puts "----------------------------------"
+  #puts four_letter_animals.push('Puma')
+  #puts "----------------------------------"
+  #puts four_letter_animals.insert(3,'Joey')
+  #puts "----------------------------------"
+  #puts four_letter_animals.delete('Dog')
+  #puts "----------------------------------"
+  #puts four_letter_animals.reverse
+  #puts "----------------------------------"
+  #puts four_letter_animals.map { |x| x == 'Elephant' ? 'Foal' : x }  
+  #puts "----------------------------------"
+  #puts four_letter_animals.push('Bear')
+  #puts "----------------------------------"
+  #puts four_letter_animals.reverse
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
